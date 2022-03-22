9286bdd4909da32093a624a6d14c1d1282d812b8
nandalestari <nndalstari27@gmail.com>
2022-03-22 17:47:35 +0700

    PROGRAM SPBU SEKUEN

---
+++ PROGRAM SPBU SEKUEN (KEL).cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+ 
+int main(){
+	int pilihan, jumlah, total, uang, kembalian;
+	int bensin;
+x:
+	cout << "##   STRUK SPBU GIWANGAN   ##\n";
+	cout << "=====================================================\n";
+	cout << "Pilihan Menu: \n";
+	cout << "[1] Kasir SPBU\n";
+	cout << "[2] Keluar dan cetak struk\n";
+	cout << "=====================================================\n";
+
+	cout << "Pilihan Anda: ";
+	cin >> pilihan;
+	if(pilihan == 1){
+		cout << "\n";
+		cout << " \t\tSPBU GIWANGAN " <<endl;
+		cout << "======================================================\n";
+		cout << "Harga perliter, PERTALITE [1]= Rp 7000\n";
+		cout << "Harga perliter, PERTAMAX [2]= Rp 9000\n";
+		cout<<"Jenis Bensin : ";
+		cin>>bensin;
+	
+}
+		
+}
