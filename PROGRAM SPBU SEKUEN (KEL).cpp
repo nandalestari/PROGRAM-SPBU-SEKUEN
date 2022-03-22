c1dd24b0feaa39b245f92ed47c1867d94d0986af
BintiSholikhah <bintisholikhah95@gmail.com>
2022-03-22 18:46:22 +0700

    tambahan kode

--- PROGRAM SPBU SEKUEN (KEL).cpp
+++ PROGRAM SPBU SEKUEN (KEL).cpp
@@ -23,7 +23,32 @@ x:
 		cout << "Harga perliter, PERTAMAX [2]= Rp 9000\n";
 		cout<<"Jenis Bensin : ";
 		cin>>bensin;
-	
 }
+	if(bensin == 1){
+		cout << "PERTALITE\n";
+		cout << "Jumlah Liter : ";
+		cin >> jumlah;
+		total = 7000*jumlah;
+		cout << "Total: Rp " << total << endl;
+		cout << "Uang yang dibayarkan : ";
+		cin >> uang;
+		kembalian = uang - total;
+		cout << "Kembalian : " << kembalian << endl;
+		cout << "=====================================================\n";
+		}
+		if(bensin == 2){
+		cout << "PERTAMAX\n";
+		cout << "Jumlah Liter : ";
+		cin >> jumlah;
+		total = 9000*jumlah;
+		cout << "Total: Rp " << total << endl;
+		cout << "Uang yang dibayarkan : ";
+		cin >> uang;
+		kembalian = uang - total;
+		cout << "Kembalian : " << kembalian << endl;
+		cout << "=====================================================\n";
+		}
+		goto x;
+	} 
 		
 }
